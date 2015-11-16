ae54e5d97fdbebc0e2e2785998512cb1b43c4ae3
gliu22 <gliu22@uic.edu>
2015-11-16 12:44:16 -0600

    dffdf

---
+++ main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  PaintingManager
+//
+//  Created by Ken Liu on 11/16/15.
+//  Copyright (c) 2015 Ken Liu. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    cout << "Hello, World!\n";
+    cout << "fsdfaf";
+    return 0;
+}
